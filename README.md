9ffe374be1bf52027343f63c969d4f8970498b51
deepesh_dhakal <deepesh@naphix.com.au>
2022-06-09 14:27:03 +0900

    First Commit:

---
+++ README.md
@@ -0,0 +1,3 @@
+__Object Oriented Programming in Wordpress__
+
+In this project, we will use object orientation for writing all logics of function.php file
---
+++ wp-content/themes/myobject/inc/enqueue-class.php
@@ -0,0 +1,23 @@
+<?php
+
+class EnqueueCustomCss
+{
+	public function __construct()
+	{
+		add_action('wp_enqueue_scripts', [$this, '__wp_enqueue_scripts'], 2, 0);
+	}
+
+	public function __wp_enqueue_scripts(): void
+	{
+		wp_enqueue_style('myobject-style', get_stylesheet_uri(), array(), _S_VERSION);
+		wp_style_add_data('myobject-style', 'rtl', 'replace');
+
+		wp_enqueue_script('myobject-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
+
+		if (is_singular() && comments_open() && get_option('thread_comments')) {
+			wp_enqueue_script('comment-reply');
+		}
+		wp_enqueue_style('myobject-custom', get_template_directory_uri() . '/css/custom.css', array(), _S_VERSION);
+	}
+}
+$enqueue_custom_css = new EnqueueCustomCss();
